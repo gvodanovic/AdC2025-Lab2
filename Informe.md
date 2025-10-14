eadd457c403367d812e600a06f76205876f034f6
gvodanovic <gonzalovodanovic@gmail.com>
2025-10-14 20:55:59 -0300

    Initial commit

---
+++ Informe.md
@@ -0,0 +1,8 @@
+# Arquitectura de Computadoras - 2025
+## Informe Lab2:  Análisis de microarquitecturas
+
+## Integrantes:
+
+ - Alumno
+ - Alumno
+ - Alumno
---
+++ benchmarks/ej_branchPred.s
@@ -0,0 +1,37 @@
+	.text
+	N:       .dword 1024	// Number of elements in the vectors
+	
+	.bss 
+
+	.arch armv8-a
+	.text
+	.align	2
+	.global	main
+	.type	main, %function
+main:
+.LFB6:
+	.cfi_startproc
+	stp	x29, x30, [sp, -16]!
+	.cfi_def_cfa_offset 16
+	.cfi_offset 29, -16
+	.cfi_offset 30, -8
+	mov	x29, sp
+//---------------------- CODE HERE ------------------------------------
+
+
+
+//---------------------- END CODE -------------------------------------
+	mov 	x0, 0
+	mov 	x1, 0
+	bl	m5_dump_stats
+	mov	w0, 0
+	ldp	x29, x30, [sp], 16
+	.cfi_restore 30
+	.cfi_restore 29
+	.cfi_def_cfa_offset 0
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
